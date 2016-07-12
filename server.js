fc4864a4d04f6e4af164747771a2643a86d3d207
Mathieu Larouche Dube <mathieu.larouche.dube@vooban.com>
2016-07-11 21:28:53 -0400

    New feature

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 app.get('/', function (req, res) {
   res.send('Hello world\n');
 });
+app.get('/new-feature', function (req, res) {
+  res.send('This is a new feature!\n');
+});
 
 var server = app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
